c3880c5185b6041b4e1b317c928ad4f90ce6550f
Umar Turdiev <ukcturdiev@gmail.com>
2023-10-20 15:39:24 -0700

    Added button controls to control the viewport start/stop

--- main.py
+++ main.py
@@ -16,9 +16,8 @@ class Object:
 
 class Viewport:
     def __init__(self):
-        self.running = True
-        self.thread = threading.Thread(target=self.simulate)
-        self.thread.start()
+        self.running = False
+        self.thread = None
 
     def simulate(self):
         while self.running:
@@ -27,29 +26,27 @@ class Viewport:
             sleep(0.5)
 
     def start(self):
-        self.running = True
+        if not self.running:
+            self.running = True
+            self.thread = threading.Thread(target=self.simulate)
+            self.thread.start()
 
-    def stop(self):
-        # Stop the simulation thread
+    def pause(self):
         self.running = False
-        if self.thread is not None:
-            self.thread = None
 
     def restart(self):
-        self.thread.join()
+        self.shutdown()
+        self.start()
 
-        self.thread = threading.Thread(target=self.simulate)
-        self.thread.start()
+    def shutdown(self):
+        self.pause()
+        self.thread.join()
 
 
 def main():
     pygame.init()
 
     viewport = Viewport()
-    # ui = UI(viewport.start, viewport.stop)
-    # ui.run()
-    # ui.ui_thread.start()
-    viewport.start()
 
     pygame.display.set_caption('Button Theming Test')
     window_surface = pygame.display.set_mode((800, 600))
@@ -59,24 +56,18 @@ def main():
     background = pygame.Surface((800, 600))
     background.fill(manager.get_theme().get_colour('dark_bg'))
 
-    load_time_1 = clock.tick()
-
-    button_row_width = 100
-    button_row_height = 40
-    spacing = 20
-    for j in range(1, 10):
-        for i in range(1, 7):
-            position = (i * spacing + ((i - 1) * button_row_width),
-                        (j * spacing + ((j - 1) * button_row_height)))
-            pygame_gui.elements.UIButton(relative_rect=pygame.Rect(position,
-                                                                   (button_row_width,
-                                                                    button_row_height)),
-                                         text=str(i) + ',' + str(j),
-                                         manager=manager,
-                                         object_id='#' + str(i) + ',' + str(j))
-
-    load_time_2 = clock.tick()
-    print('Button creation time taken:', load_time_2 / 1000.0, 'seconds.')
+    start_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((10, 10), (100, 30)),
+                                                text='Start',
+                                                manager=manager,
+                                                object_id='Start')
+    pause_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((120, 10), (100, 30)),
+                                                text='Pause',
+                                                manager=manager,
+                                                object_id='Pause')
+    restart_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((240, 10), (100, 30)),
+                                                  text='Restart',
+                                                  manager=manager,
+                                                  object_id='Restart')
 
     is_running = True
 
@@ -84,7 +75,21 @@ def main():
         time_delta = clock.tick(60) / 1000.0
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                viewport.shutdown()
                 is_running = False
+
+            if event.type == pygame_gui.UI_BUTTON_PRESSED:
+                if event.ui_element == start_button:
+                    print('Start button pressed')
+                    viewport.start()
+
+                if event.ui_element == pause_button:
+                    print('Pause button pressed')
+                    viewport.pause()
+
+                if event.ui_element == restart_button:
+                    print('Restart button pressed')
+                    viewport.restart()
 
             manager.process_events(event)
 
