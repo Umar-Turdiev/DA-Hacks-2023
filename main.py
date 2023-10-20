771f2ac66e75feb10246c8756821870bcbd8eb92
Umar Turdiev <ukcturdiev@gmail.com>
2023-10-20 16:17:55 -0700

    Added box drawing

--- main.py
+++ main.py
@@ -2,16 +2,25 @@ from time import sleep
 
 import pygame
 import pymunk
+import pymunk.pygame_util
 import pygame_gui
 import threading
+
+pygame.init()
+pygame.display.set_caption('Button Theming Test')
+window_surface = pygame.display.set_mode((800, 600))
+manager = pygame_gui.UIManager((800, 600), 'data/themes/button_theming_test_theme.json')
+clock = pygame.time.Clock()
+
+background = pygame.Surface((800, 600))
+background.fill(manager.get_theme().get_colour('dark_bg'))
 
 scene_objects = {}
 
 
 class Object:
     def __init__(self, shape_data):
-        # Initialize object properties using shape data
-        self.shape_data = shape_data
+        pass
 
 
 class Viewport:
@@ -19,11 +28,27 @@ class Viewport:
         self.running = False
         self.thread = None
 
+        self.space = pymunk.Space()  # Create a Space which contain the simulation
+        self.space.gravity = 0, -981
+
+        self.body = pymunk.Body()  # Create a Body
+        self.body.position = 50, 100  # Set the position of the body
+
+        self.poly = pymunk.Poly.create_box(self.body)  # Create a box shape and attach to body
+        self.poly.mass = 10  # Set the mass on the shape
+        self.space.add(self.body, self.poly)
+
     def simulate(self):
+        draw_options = pymunk.pygame_util.DrawOptions(background)
+        # draw_options = pymunk.SpaceDebugDrawOptions()  # For easy printing
+
         while self.running:
-            # replace this to simulate the objects from the list
-            print('sim')
-            sleep(0.5)
+            self.space.step(0.02)
+
+            background.fill(manager.get_theme().get_colour('dark_bg'))
+            self.space.debug_draw(draw_options)  # Print the state of the simulation
+
+            sleep((1000.0 / 60) / 1000)
 
     def start(self):
         if not self.running:
@@ -44,17 +69,7 @@ class Viewport:
 
 
 def main():
-    pygame.init()
-
     viewport = Viewport()
-
-    pygame.display.set_caption('Button Theming Test')
-    window_surface = pygame.display.set_mode((800, 600))
-    manager = pygame_gui.UIManager((800, 600), 'data/themes/button_theming_test_theme.json')
-    clock = pygame.time.Clock()
-
-    background = pygame.Surface((800, 600))
-    background.fill(manager.get_theme().get_colour('dark_bg'))
 
     start_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((10, 10), (100, 30)),
                                                 text='Start',
