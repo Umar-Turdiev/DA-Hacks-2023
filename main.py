e4b2958fbcbf6768072f2294542801a3c82f85f1
Umar Turdiev <ukcturdiev@gmail.com>
2023-10-20 23:32:49 -0700

    Add function to add shape to scene

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # Psysicks - DAHacks Project
-
+from enum import Enum
 from time import sleep
 
 import pygame
 import pymunk
+import pymunk.util
 import pymunk.pygame_util
 from pymunk import Vec2d
 import pygame_gui
@@ -12,22 +13,31 @@ import threading
 COLLTYPE_DEFAULT = 0
 COLLTYPE_MOUSE = 1
 
+WIDTH = 800
+HEIGHT = 600
+
 pygame.init()
 pygame.display.set_caption('Pysicks')
-window_surface = pygame.display.set_mode((800, 600))
-manager = pygame_gui.UIManager((800, 600), 'data/themes/button_theming_test_theme.json')
+window_surface = pygame.display.set_mode((WIDTH, HEIGHT))
+manager = pygame_gui.UIManager((WIDTH, HEIGHT), 'data/themes/button_theming_test_theme.json')
 clock = pygame.time.Clock()
 
 background = pygame.Surface((800, 600))
 background.fill(manager.get_theme().get_colour('dark_bg'))
 
-scene_objects = {}
+scene_objects = []
 
 
 class Object:
-    def __init__(self, body, poly):
+    def __init__(self, type, body, poly):
+        self.type = type
         self.body = body
         self.poly = poly
+
+    class Type(Enum):
+        BALL = 1
+        POLY = 2
+        WALL = 3
 
 
 class Viewport:
@@ -38,24 +48,7 @@ class Viewport:
         self.space = pymunk.Space()  # Create a Space which contain the simulation
         self.space.gravity = 0, -981
 
-        self.body = pymunk.Body()  # Create a Body
-        self.body.position = 50, 100  # Set the position of the body
-
-        self.poly = pymunk.Poly.create_box(self.body)  # Create a box shape and attach to body
-        self.poly.mass = 10  # Set the mass on the shape
-        self.space.add(self.body, self.poly)
-
-        # self.add_box(200, 1100, 100, 3)
-
-        ### Init pygame and create screen
-        pygame.init()
-        self.w, self.h = 600, 600
-        self.screen = pygame.display.set_mode((self.w, self.h))
         self.clock = pygame.time.Clock()
-
-        ### Init pymunk and create space
-        self.space = pymunk.Space()
-        self.space.gravity = (0.0, -900.0)
 
         ### Walls
         self.walls = []
@@ -67,13 +60,6 @@ class Viewport:
 
         ### Polys
         self.polys = []
-        h = 10
-        for y in range(1, h):
-            # for x in range(1, y):
-            x = 0
-            s = 10
-            p = Vec2d(300, 40) + Vec2d(0, y * s * 2)
-            self.polys.append(self.create_box(p, size=s, mass=1))
 
         self.run_physics = True
 
@@ -85,31 +71,26 @@ class Viewport:
         self.shape_to_remove = None
         self.mouse_contact = None
 
-        self.create_ball((500, 500))
+        # self.balls.append(self.create_ball((500, 500)))
+        scene_objects.append(self.create_ball((500, 500)))
 
-    def detect_click(self, x, y):
-        for circle, circle_info in self.balls:
-            if circle.point_query((x, 600 - y)):
-                print("Circle clicked!")
-
-    def add_box(self, x, y, width, height, mass=10):
-        body = pymunk.Body()
-        body.position = x, y
-        poly = pymunk.Poly.create_box(body, (width, height))
-        poly.mass = mass
-        self.space.add(body, poly)
+    def flipyv(self, v):
+        return int(v.x), int(-v.y + HEIGHT)
 
     def create_ball(self, point, mass=1.0, radius=15.0):
         moment = pymunk.moment_for_circle(mass, 0.0, radius)
         ball_body = pymunk.Body(mass, moment)
         ball_body.position = Vec2d(*point)
+        ball_body.velocity = -100, 500
 
         ball_shape = pymunk.Circle(ball_body, radius)
         ball_shape.friction = 1.5
         ball_shape.collision_type = COLLTYPE_DEFAULT
         self.space.add(ball_body, ball_shape)
 
-        return ball_shape
+        object = Object(Object.Type.BALL, ball_body, ball_shape)
+
+        return object
 
     def create_box(self, pos, size=10, mass=5.0):
         box_points = [(-size, -size), (-size, size), (size, size), (size, -size)]
@@ -127,7 +108,11 @@ class Viewport:
         body.velocity = 10, 2
         self.space.add(body, shape)
 
-        return shape
+        object = Object(Object.Type.POLY, body, shape)
+
+        scene_objects.append(object)
+
+        return object
 
     def create_wall_segments(self, points):
         """Create a number of wall segments connecting the points"""
@@ -142,65 +127,61 @@ class Viewport:
             wall_shape.friction = 1.0
             wall_shape.collision_type = COLLTYPE_DEFAULT
             self.space.add(wall_body, wall_shape)
-            self.walls.append(wall_shape)
+
+            scene_objects.append(Object(Object.Type.WALL, wall_body, wall_shape))
 
     def draw_ball(self, ball):
         body = ball.body
         v = body.position + ball.offset.cpvrotate(body.rotation_vector)
         p = self.flipyv(v)
         r = ball.radius
-        pygame.draw.circle(self.screen, pygame.Color("blue"), p, int(r), 2)
+        pygame.draw.circle(background, pygame.Color("blue"), p, int(r), 2)
 
     def draw_wall(self, wall):
         body = wall.body
         pv1 = self.flipyv(body.position + wall.a.cpvrotate(body.rotation_vector))
         pv2 = self.flipyv(body.position + wall.b.cpvrotate(body.rotation_vector))
-        pygame.draw.lines(self.screen, pygame.Color("lightgray"), False, [pv1, pv2])
+        pygame.draw.lines(background, pygame.Color("lightgray"), False, [pv1, pv2])
 
     def draw_poly(self, poly):
         body = poly.body
         ps = [p.rotated(body.angle) + body.position for p in poly.get_vertices()]
         ps.append(ps[0])
         ps = list(map(self.flipyv, ps))
-        if u.is_clockwise(ps):
+        if pymunk.util.is_clockwise(ps):
             color = pygame.Color("green")
         else:
             color = pygame.Color("red")
-        pygame.draw.lines(self.screen, color, False, ps)
+        pygame.draw.lines(background, color, False, ps)
 
     def draw(self):
-        ### Clear the screen
-        self.screen.fill(pygame.Color("white"))
+        # Clear the screen
+        background.fill(manager.get_theme().get_colour('dark_bg'))
 
-        ### Display some text
-        self.draw_helptext()
+        for object in scene_objects:
+            if object.type == Object.Type.BALL:
+                self.draw_ball(object.poly)
 
-        ### Draw balls
-        for ball in self.balls:
-            self.draw_ball(ball)
+            if object.type == Object.Type.POLY:
+                self.draw_poly(object.poly)
 
-        ### Draw walls
-        for wall in self.walls:
-            self.draw_wall(wall)
-
-        ### Draw polys
-        for poly in self.polys:
-            self.draw_poly(poly)
+            if object.type == Object.Type.WALL:
+                self.draw_wall(object.poly)
 
         ### Draw Uncompleted walls
         if len(self.wall_points) > 1:
             ps = [self.flipyv(Vec2d(*p)) for p in self.wall_points]
-            pygame.draw.lines(self.screen, pygame.Color("gray"), False, ps, 2)
+            pygame.draw.lines(background, pygame.Color("gray"), False, ps, 2)
 
         ### Uncompleted poly
         if len(self.poly_points) > 1:
             ps = [self.flipyv(Vec2d(*p)) for p in self.poly_points]
-            pygame.draw.lines(self.screen, pygame.Color("red"), False, ps, 2)
+            pygame.draw.lines(background, pygame.Color("red"), False, ps, 2)
 
         ### Mouse Contact
         if self.mouse_contact is not None:
             p = self.flipyv(self.mouse_contact)
-            pygame.draw.circle(self.screen, pygame.Color("red"), p, 3)
+            pygame.draw.circle(background, pygame.Color("red"), p, 3)
 
         ### All done, lets flip the display
         pygame.display.flip()
@@ -209,19 +190,23 @@ class Viewport:
         draw_options = pymunk.pygame_util.DrawOptions(background)
         # draw_options = pymunk.SpaceDebugDrawOptions()  # For easy printing
 
-        while self.running:
-            self.space.step(0.02)
+        self.space.step(0.02)
 
-            background.fill(manager.get_theme().get_colour('dark_bg'))
-            self.space.debug_draw(draw_options)  # Print the state of the simulation
+        # self.space.debug_draw(draw_options)  # Print the state of the simulation
+        self.draw()
 
-            sleep((1000.0 / 60) / 1000)
+        # while self.running:
+        #     self.space.step(0.02)
+        #
+        #     # self.space.debug_draw(draw_options)  # Print the state of the simulation
+        #     self.draw()
+        #     clock.tick(50)
 
     def start(self):
         if not self.running:
             self.running = True
-            self.thread = threading.Thread(target=self.simulate)
-            self.thread.start()
+            # self.thread = threading.Thread(target=self.simulate)
+            # self.thread.start()
 
     def pause(self):
         self.running = False
@@ -251,6 +236,11 @@ def main():
                                                   manager=manager,
                                                   object_id='Restart')
 
+    add_rect = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((10, 300), (100, 30)),
+                                            text='Add Rectangle',
+                                            manager=manager,
+                                            object_id='AddRectangle')
+
     is_running = True
 
     while is_running:
@@ -262,8 +252,7 @@ def main():
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:  # left click
-                    x, y = pygame.mouse.get_pos()
-                    viewport.detect_click(x, y)
+                    pass
 
             if event.type == pygame_gui.UI_BUTTON_PRESSED:
                 if event.ui_element == start_button:
@@ -278,12 +267,19 @@ def main():
                     print('Restart button pressed')
                     viewport.restart()
 
+                if event.ui_element == add_rect:
+                    scene_objects.append(viewport.create_ball((500, 500)))
+                    viewport.create_box((500, 500))
+
             manager.process_events(event)
 
         manager.update(time_delta)
 
         window_surface.blit(background, (0, 0))
         manager.draw_ui(window_surface)
+
+        if viewport.running:
+            viewport.simulate()
 
         pygame.display.update()
 
